00927d85094f31b0fd72a4ebdd2f3a23c3d1f270
Gabriel Staples <ercaguy@gmail.com>
2020-03-22 12:35:28 -0700

    Structure git-tree.py file with a class & main() func

--- git-tree.py
+++ git-tree.py
@@ -3,9 +3,35 @@
 """
 This file is part of git-tree: https://github.com/ElectricRCAircraftGuy/git-tree
 Author: Gabriel Staples
+
+References:
+Here's a few of my own Python code samples (previous projects) I am looking at to remind myself how to write
+Python:
+1. More C++-like (uses a class): https://github.com/ElectricRCAircraftGuy/LibreBulletin/blob/master/bulletin_find_and_replace.py
+2. More C-like (does NOT use a class): https://github.com/ElectricRCAircraftGuy/eRCaGuy_PyTerm/blob/master/serial_terminal.py
+
 """
 
-print("hello")
+# Imports
+
+class GitTree:
+
+    def __init__(self):
+        pass
 
 
+    def tree(self):
+        pass
 
+    def cascade(self):
+        pass
+
+# end of class GitTree
+
+def main():
+    print("Running main") # for debugging
+
+
+# If this file is called directly, as opposed to imported, do this:
+if __name__ == '__main__':
+    main()
