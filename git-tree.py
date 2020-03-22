e7d025e74f92ff7e2e55a16f315aa48cb74d9dec
Gabriel Staples <ercaguy@gmail.com>
2020-03-22 12:40:45 -0700

    Add logger to do debug logging

--- git-tree.py
+++ git-tree.py
@@ -5,14 +5,22 @@ This file is part of git-tree: https://github.com/ElectricRCAircraftGuy/git-tree
 Author: Gabriel Staples
 
 References:
+
 Here's a few of my own Python code samples (previous projects) I am looking at to remind myself how to write
 Python:
 1. More C++-like (uses a class): https://github.com/ElectricRCAircraftGuy/LibreBulletin/blob/master/bulletin_find_and_replace.py
 2. More C-like (does NOT use a class): https://github.com/ElectricRCAircraftGuy/eRCaGuy_PyTerm/blob/master/serial_terminal.py
 
+Logging:
+1. https://inventwithpython.com/blog/2012/04/06/stop-using-print-for-debugging-a-5-minute-quickstart-guide-to-pythons-logging-module/
+
 """
 
 # Imports
+import logging
+
+# Globals
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(message)s')
 
 class GitTree:
 
@@ -29,7 +37,7 @@ class GitTree:
 # end of class GitTree
 
 def main():
-    print("Running main") # for debugging
+    logging.debug('Running main.')
 
 
 # If this file is called directly, as opposed to imported, do this:
